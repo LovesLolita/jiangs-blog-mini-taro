d235fd25a0d67fa48572b10642e83343642f758f
BluesMa <maxiaowl@foxmail.com>
2023-04-21 18:05:11 +0800

    edit

--- src/pages/hot/hot.jsx
+++ src/pages/hot/hot.jsx
@@ -36,9 +36,6 @@ const Hot = () => {
     { title: "评论数", sort: "comments" },
   ];
   const [tabValue, setTabValue] = useState("views");
-  useEffect(() =>{
-    getHotArticles('refresh')
-  } ,[tabValue])
   /* tab栏end */
 
   /* 热门文章列表 */
@@ -60,7 +57,7 @@ const Hot = () => {
     } catch (err) {
       console.log(err);
     }
-  }, []);
+  }, [tabValue]);
   useEffect(() => {
     getHotArticles();
   }, [getHotArticles]);
