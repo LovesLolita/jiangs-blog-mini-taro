ceb7e01893ae3f12204c81cd9f66089a267d5a14
BluesMa <maxiaowl@foxmail.com>
2023-06-06 17:35:10 +0800

    edit 删除redux react-redux dva 相关依赖 提升react 版本为 18.2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 import { Component } from 'react'
-import { Provider } from 'react-redux'
-import createApp from './dva'
-import modules from './store'
+
+
 import './app.scss'
 
 
-const dvaApp = createApp({
-  initialState: {},
-  modules,
-})
-const store = dvaApp.getStore();
+
+
 class App extends Component {
 
   componentDidMount () {}
@@ -20,7 +16,7 @@ class App extends Component {
 
   render () {
     // this.props.children 是将要会渲染的页面
-    return <Provider store={store}>{this.props.children}</Provider>
+    return this.props.children
   }
 }
 
--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import flightIndex from '@/store/module/flightIndex';
-// import user from './user'
-
-export default [
-  flightIndex,
-  // user
-]
